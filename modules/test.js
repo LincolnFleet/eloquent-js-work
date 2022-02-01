c8efcf7a21066d9d13ce4a6cb715f2159b233453
Travis Martin <travisemerymartin@gmail.com>
2022-02-01 00:23:03 -0500

    Update test.unit

--- modules/test.js
+++ modules/test.js
@@ -6,25 +6,29 @@ module.exports = { unit };
 
 /**
  * Function to test a subject function against expected output.
- * @param {Object} test - {label, subject, inputArgs, expectedOutput}
+ * @param {Object} test - {label, subject, params, expectedOutput}
  * @param {string} test.label - Name for specific test or test run.
  * @param {Function} test.subject - Function to be tested.
- * @param {*} test.inputArgs - Value or list of arguments to be passed to the subject.
- * @param {*} test.expectedOutput - Expected return value from subject when executed using inputArgs.
+ * @param {*} test.params - Value or list of arguments to be passed to the subject.
+ * @param {*} test.expectedOutput - Expected return value from subject when executed using params.
  * @return {Boolean} - Indicates true if test has passed and false if not.
  */
-function unit({ label, subject, inputArgs, expectedOutput }) {
-	// ensure inputArgs is an interable
-	if (!(inputArgs instanceof Array)) inputArgs = new Array(inputArgs);
-
-	const hasPassed = subject(...inputArgs) == expectedOutput;
-
-	console.log(""); // line break for readability
-	console.log(label + ":", hasPassed ? "PASS" : "FAIL");
-	console.log("inputArgs: ", JSON.stringify(inputArgs));
-	console.log("expected out: ", JSON.stringify(expectedOutput));
-	!hasPassed && console.log("actual out: ", JSON.stringify(subject));
+function unit({ label, subject, params, expectedOutput }) {
 	console.log(""); // line break for readability
 
+	// ensure params is an interable
+	if (!(params instanceof Array)) params = new Array(params);
+	const actualOutput = subject(...params);
+	const hasPassed = actualOutput === expectedOutput;
+
+	hasPassed
+		? console.log(label + ": " + "%cPass", "color:blue;")
+		: console.log(label + ": " + "%cFAIL", "color:orange;");
+	// console.log(label + ":", hasPassed ? "PASS" : "FAIL");
+	console.log("params:", params);
+	console.log("expected out:", expectedOutput);
+	!hasPassed && console.log("actual out:", actualOutput);
+
+	console.log(""); // line break for readability
 	return hasPassed;
 };
